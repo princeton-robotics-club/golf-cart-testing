51c594a2c2df58df963a21734a48f2989909641a
Bereketab <btessema@princeton.edu>
2023-11-18 20:28:14 -0500

    Test

--- segmentation_test_loop.py
+++ segmentation_test_loop.py
@@ -25,13 +25,13 @@ transform = T.Compose([
 # Load and transform the image
 original_img1 = cv2.imread('street2.jpg')
 img1 = cv2.cvtColor(original_img1, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-img1 = cv2.resize(img1, (2688, 1520))
-img1 = transform(img1).unsqueeze(0).to(device)
+img1_t = cv2.resize(img1, (2688/4, 1520/4))
+img1 = transform(img1_t).unsqueeze(0).to(device)
 
 original_img2 = cv2.imread('street3.jpg')
 img2 = cv2.cvtColor(original_img2, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-img2 = cv2.resize(img2, (2688, 1520))
-img2 = transform(img2).unsqueeze(0).to(device)
+img2_t = cv2.resize(img2, (2688/4, 1520/4))
+img2 = transform(img2_t).unsqueeze(0).to(device)
 
 # Perform segmentation
 with torch.no_grad():
@@ -58,7 +58,7 @@ road_mask = (preds == 0).astype('uint8')
 road_mask = np.squeeze(road_mask, axis=0)
 
 # # Apply the binary mask to the original image
-# masked_image = cv2.bitwise_and(original_img, original_img, mask=road_mask)
+# masked_image = cv2.bitwise_and(, original_img, mask=road_mask)
 # #print(torch.cuda.memory_allocated()/(1024*1024))
 # cv2.imshow('Masked Image', cv2.resize(masked_image, (int(masked_image.shape[1]/2), int(masked_image.shape[0]/2))))
 # cv2.waitKey(0)
