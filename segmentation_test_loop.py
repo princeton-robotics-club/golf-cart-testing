5d00f4de8b42e097afa0fb294980d6a6e5642663
Bereketab <btessema@princeton.edu>
2023-11-18 20:32:32 -0500

    test

--- segmentation_test_loop.py
+++ segmentation_test_loop.py
@@ -25,12 +25,12 @@ transform = T.Compose([
 # Load and transform the image
 original_img1 = cv2.imread('street2.jpg')
 img1 = cv2.cvtColor(original_img1, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-img1 = cv2.resize(img1, (2688/4, 1520/4))
+img1 = cv2.resize(img1, (int(2688/4), int(1520/4)))
 img1 = transform(img1).unsqueeze(0).to(device)
 
 original_img2 = cv2.imread('street3.jpg')
 img2 = cv2.cvtColor(original_img2, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-img2 = cv2.resize(img2, (2688/4, 1520/4))
+img2 = cv2.resize(img2, (int(2688/4), int(1520/4)))
 img2 = transform(img2).unsqueeze(0).to(device)
 
 # Perform segmentation
